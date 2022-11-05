e0c456941d8e6ea1b6a0d50571b136364b05d9da
Mamudu Kamilo <kamelodee@gmail.com>
2022-11-05 12:58:59 +0000

    pwa app

--- resources/views/layouts/layout1.blade.php
+++ resources/views/layouts/layout1.blade.php
@@ -8,7 +8,7 @@
 <title>hi {{Auth::user()->name}}</title>
 <meta name="description" content="HisenseGh payment gatewy">
 <meta name="author" content="harnishdesign.net">
-
+@laravelPWA
 <!-- Web Fonts
 ============================================= -->
 <link rel="stylesheet" href="../../../css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">
@@ -20,7 +20,7 @@
 <link rel="stylesheet" type="text/css" href="{{asset("assets1/vendor/font-awesome/css/all.min.css")}}">
 <link rel="stylesheet" type="text/css" href="{{asset("assets1/css/stylesheet.css")}}">
 <!-- Colors Css -->
-@laravelPWA
+
 </head>
 <body>
 
