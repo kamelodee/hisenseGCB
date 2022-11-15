53875d1ed84b8ea94513d110d56e845ca0665c5e
Mamudu Kamilo <kamelodee@gmail.com>
2022-11-15 13:19:45 +0000

    test

--- app/Models/TestTransaction.php
+++ app/Models/TestTransaction.php
@@ -4,8 +4,51 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class TestTransaction extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'customer_name',
+        'showroom',
+        'order_code',
+        'payment_token',
+        'payment_code',
+        'shortpay_code',
+        'transaction_id',
+        'transaction_type',
+        'ref',
+        'phone',
+        'account_number',
+        'description',
+        'date',
+        'amount',
+        'status',
+        'bank',
+        'sales_reference_id',
+        
+    ];
+
+    public function getTransations($bank)
+    {
+        $calbank = Transaction::where('bank',$bank)->get();
+        return  $calbank;
+    }
+    public static function getCashiertransations($bank)
+    {
+        $calbank = self::where('bank',$bank)->where('showroom',Auth::user()->showroom)->get();
+        return  $calbank;
+    }
+    public static function transations($bank)
+    {
+        $calbank =self::where('bank',$bank)->where('status', 'SUCCESS')->sum('amount');
+        return  $calbank;
+    }
+
+    public static function cashiertransation($bank)
+    {
+        $calbank =self::where('bank',$bank)->where('showroom',Auth::user()->showroom)->where('status', 'SUCCESS')->sum('amount');
+        return  $calbank;
+    }
 }
