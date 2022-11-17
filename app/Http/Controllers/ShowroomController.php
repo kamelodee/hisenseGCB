0158abb02d3238cac58b7d5fe6343a2b4a0bdef0
Mamudu Kamilo <kamelodee@gmail.com>
2022-11-17 10:54:49 +0000

    calbanl

--- app/Http/Controllers/ShowroomController.php
+++ app/Http/Controllers/ShowroomController.php
@@ -56,8 +56,15 @@ class ShowroomController extends Controller
         $showrooms = Showroom::all();
         foreach($showrooms as $showroom){
            $sum = Transaction::where('showroom',$showroom->name)->whereIn('status', ['SUCCESS','SUCCESSFUL'])->sum('amount');
+           $gcbsum = Transaction::where('showroom',$showroom->name)->where('bank','GCB')->whereIn('status', ['SUCCESS','SUCCESSFUL'])->sum('amount');
+           $momo = Transaction::where('showroom',$showroom->name)->where('transaction_type','momo')->whereIn('status', ['SUCCESS','SUCCESSFUL'])->sum('amount');
+           $card = Transaction::where('showroom',$showroom->name)->where('bank','UBA')->whereIn('status', ['SUCCESS','SUCCESSFUL'])->sum('amount');
            if($sum ){
-               $showroom->update(['total'=>$sum]);
+               $showroom->update([
+                'total'=>$sum,
+                'gcb'=>$gcbsum,
+                'momo'=>$momo
+            ]);
            }
         
         }
