c5016213e9062da8c245412278fb07cad2161fa9
Mamudu Kamilo <kamelodee@gmail.com>
2022-11-17 10:10:14 +0000

    calbanl

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,10 @@ use App\Http\Controllers\GcbController;
 
 
 Route::middleware(['whitelist'])->group(function () {
+    Route::match(['post','get'],'/hisense/payment/reconcile', [GcbController::class, 'deposit'])->middleware('auth:sanctum');
+    Route::match(['post','get'],'/verify/payment/reconcile', [GcbController::class, 'deposit'])->middleware('auth:sanctum');
     Route::match(['post','get'],'/payment/reconcile', [GcbController::class, 'deposit'])->middleware('auth:sanctum');
+    Route::post('/payment', [GcbController::class, 'login']);
     Route::post('/payment/login', [GcbController::class, 'login']);
 
     Route::match(['post','get'],'ecobank/payment/reconcile', [GcbController::class, 'ecobankdeposit'])->middleware('auth:sanctum');
